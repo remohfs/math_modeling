40fb89f1809edfa5a0c6a98bc2db54bf5227df8c
remohfs <146558165+remohfs@users.noreply.github.com>
2023-10-07 16:17:42 +0000

    ff

---
+++ 1.py
@@ -0,0 +1,7 @@
+a = 3
+b = 4
+x = a + b
+print('Hello, World!')
+print('Маша + Петя = Любовь')
+print(f'x = {a} + {b}')
+print(f'x = {x}')
---
+++ 6.py
@@ -0,0 +1,13 @@
+
+print('возраст')
+a = input()
+print('пол')
+b = input()
+print('имя')
+c = input()
+print('город')
+d = input()
+print('класс')
+f = input()
+x = [a, b , c, d, f]
+print(x)
